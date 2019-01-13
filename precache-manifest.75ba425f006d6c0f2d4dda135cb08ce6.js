dbfabb1ce27b6562f241aa6bf939718084cfc864
kyle_rosenmeyer <kylerosenmeyer@gmail.com>
2019-01-12 22:26:36 -0500

    Updates

---
+++ precache-manifest.75ba425f006d6c0f2d4dda135cb08ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d12073176d962c018009eb7176fd5d9",
+    "url": "/memory-game/static/media/hypnotize.3d120731.png"
+  },
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "5a7e619ddaecda14572c",
+    "url": "/memory-game/static/js/main.5a7e619d.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/memory-game/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "5a7e619ddaecda14572c",
+    "url": "/memory-game/static/css/main.e1fe6c63.chunk.css"
+  },
+  {
+    "revision": "e0656b348ca848dcb34607a65b52a720",
+    "url": "/memory-game/index.html"
+  }
+];
